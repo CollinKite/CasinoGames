c782ab71dcc9d2d2a94322310d39777dddf5ef4a
Lissetx <98356241+Lissetx@users.noreply.github.com>
2023-01-31 14:02:07 -0700

    Update Player.java
    our main player class

--- Games/src/Model/Player.java
+++ Games/src/Model/Player.java
@@ -1,7 +1,28 @@
+package Model;
+
 public class Player{
     private String name;
 
     private int credits;
 
-    
+    public Player(String name, int credits) {
+        this.name = name;
+        this.credits = credits;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getCredits() {
+        return credits;
+    }
+
+    public void setCredits(int credits) {
+        this.credits = credits;
+    }
 }
