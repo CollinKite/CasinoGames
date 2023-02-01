ea14f1aeb3e296f895923ef4437aa53d761176a6
Lissetx <98356241+Lissetx@users.noreply.github.com>
2023-02-01 13:47:23 -0700

    BJ menu
    menu

--- Games/src/BlackJack/View/BlackJackView.java
+++ Games/src/BlackJack/View/BlackJackView.java
@@ -1,4 +1,16 @@
 package BlackJack.View;
 
+import Model.Player;
+
 public class BlackJackView {
+    public void slotsMenu(Player player) {
+        System.out.println("♣♠ Welcome to the Casino Black! ♥♦");
+        System.out.println("You have $" + player.getCredits() + " credits");
+        System.out.println("Each spin costs $50");
+        System.out.println("Please select an option:");
+        System.out.println("1. Play Blackjack");
+        System.out.println("2. Exit");
+    }
+
+
 }
